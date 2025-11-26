4033cd3a66f932225ede1a44e8172ba7f20bb1f8
Ayzada Baychabaeva <ayzada.baychabaeva22@gmail.com>
2025-11-26 17:49:03 +0600

    add theme

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color black = Color(0xff000000);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color blue = Color(0xff0033DA);
+  static const Color yellow = Color(0xffF4E869);
+  static const Color grey = Color(0xffF6F6F6);
+  static const Color grey2 = Color(0xffE8E8E8);
+}
